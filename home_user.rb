31aacbae04442ede9f9d47326a4b5eb5573a59f6
ramtruely <ramtruely6@gmail.com>
2017-07-26 22:16:07 +0530

    modified the file home_user

--- home_user.rb
+++ home_user.rb
@@ -2,3 +2,15 @@ user 'myuser' do
   supports :manage_home => true
   home '/home/myuser'
 end
+user 'testuser' do
+  supports :manage_home => true
+  home '/home/testuser'
+  shell 'sbin/nologin'
+end 
+user 'firstuser' do
+  supports :manage_home => true
+  home '/home/firstuser'
+  password '$1$oJbMObxR$8YGbtshOA5/FNAOX1HVuO1'
+  shell '/bin/bash'     #This resource has successfully executed
+end
+ 
